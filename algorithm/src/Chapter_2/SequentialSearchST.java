1f01746a3dfc9c57b41edf24d1cc6e5105ab18ff
xiantang <986494553@qq.com>
2018-08-29 20:49:50 +0800

    add delete

--- algorithm/src/Chapter_2/SequentialSearchST.java
+++ algorithm/src/Chapter_2/SequentialSearchST.java
@@ -16,17 +16,52 @@ public class SequentialSearchST<Key, Value> {
             this.next = next;
         }
 
-        public Value get(Key key) {
-            for (Node x = first; x != null; x = x.next) {
-                if (key.equals(x.key)) return x.value;
-            }
-            return null;
+
+    }
+
+    public Value get(Key key) {
+        for (Node x = first; x != null; x = x.next) {
+            if (key.equals(x.key)) return x.value;
         }
-        public void put(Key key,Value val){
-            for (Node x = first; x != null; x = x.next) {
-                if (key.equals(x.key)) x.value = val; return;
-            }
-            first = new Node(key,val,first);
+        return null;
+    }
+
+    public void put(Key key, Value val) {
+        for (Node x = first; x != null; x = x.next) {
+            if (key.equals(x.key)) x.value = val;
+            return;
         }
+        first = new Node(key, val, first);
+    }
+
+    public int size() {
+        int i = 0;
+        for (Node x = first; x != null; x = x.next) {
+            i += 1;
+        }
+        return i;
+    }
+
+    public void delete(Key key) {
+        Node y = null;
+        for (Node x = first; x != null; x = x.next) {
+            if (key.equals(x.key)) {
+                if (y == null) {
+                    first = x.next;
+                } else y.next = x.next;
+            }
+            y = x;
+        }
+    }
+
+    public static void main(String[] args) {
+        SequentialSearchST sequentialSearchST = new SequentialSearchST<String, Integer>();
+        sequentialSearchST.put("qq",1);
+        sequentialSearchST.put("qqq",1);
+        sequentialSearchST.put("qqqa",1);
+        sequentialSearchST.delete("qq");
+        System.out.println(sequentialSearchST.get("qq"));
+        sequentialSearchST.delete("qqqa");
+
     }
 }
