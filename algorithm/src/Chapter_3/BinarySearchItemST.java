6cbf9439e92da4c2300565bd9222bc6ceb187d8c
zhujingdi <34479567+xiantang@users.noreply.github.com>
2018-09-01 22:28:23 +0800

    get error with item

---
+++ algorithm/src/Chapter_3/BinarySearchItemST.java
@@ -0,0 +1,30 @@
+package Chapter_3;
+
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class BinarySearchItemST<Key extends Comparable<Key>,Value> {
+
+    private Map<Key,Value>[] item;
+
+    public BinarySearchItemST(Map<Key,Value>[] maps){
+
+
+
+    }
+
+
+
+
+    public static void main(String[] args) {
+        Map<String,Integer>[] maps= new  Map[10];
+        for (int i = maps.length-1; i >0 ; i--) {
+            Map<String,Integer> map = new HashMap<String, Integer>();
+            map.put(""+i,i);
+            maps[i] = map;
+        }
+
+        new BinarySearchItemST<String,Integer>(maps);
+    }
+}
