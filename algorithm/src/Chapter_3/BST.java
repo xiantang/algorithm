b0967f970d529e0ea7d13bd3ea62d4a621c25d5b
dd <986494553@qq.com>
2018-09-09 19:26:55 +0800

    add bst pirntGraph()

--- algorithm/src/Chapter_3/BST.java
+++ algorithm/src/Chapter_3/BST.java
@@ -2,6 +2,11 @@ package Chapter_3;
 
 //import org.w3c.dom.Node;
 
+import algs4.Queue;
+
+import javax.xml.soap.Node;
+import java.util.ArrayList;
+
 public class BST<Key extends Comparable<Key>, Value> {
     private Node root;
 
@@ -109,49 +114,86 @@ public class BST<Key extends Comparable<Key>, Value> {
     private void delete(Key key) {
         root = delete(root, key);
     }
-    private Node delete(Node node,Key key){
-        if (node==null)return null;
+
+    private Node delete(Node node, Key key) {
+        if (node == null) return null;
         int cmp = key.compareTo(node.key);
-        if (cmp<0) node.left = delete(node.left,key);
-        if (cmp>0) node.right = delete(node.right,key);
+        if (cmp < 0) node.left = delete(node.left, key);
+        if (cmp > 0) node.right = delete(node.right, key);
         else {
-            if (node.right == null)return node.left;
+            if (node.right == null) return node.left;
             if (node.left == null) return node.right;
-            Node x=node;
+            Node x = node;
             node = min(x.left);
             node.right = deleteMin(x.right);
             node.left = x.left;
 
         }
-        node.N = size(node.left)+size(node.right)+1;
+        node.N = size(node.left) + size(node.right) + 1;
         return node;
-    }    private Node min(Node node){
-        if (node.left==null){
+    }
+
+    private Node min(Node node) {
+        if (node.left == null) {
             return node;
-        }else {
+        } else {
             return min(node.left);
         }
 
     }
 
+    private void print() {
+        print(root);
+    }
 
 
-//    private Node delete(Node x, Key key) {
-//        if (x == null) return null;
-//        int cmp = key.compareTo(x.key);
-//        if (cmp < 0) x.left = delete(x.left, key);
-//        if (cmp > 0) x.right = delete(x.right, key);
-//        else {
-//            if (x.right == null) return x.left;
-//            if (x.left == null) return x.right;
-//            Node t = x;
-//            x=min(t.left);
-//            x.right = deleteMin(t.right);
-//            x.left = t.left;
-//        }
-//        x.N = size(x.left)+size(x.right)+1;
-//        return x;
-//    }
+    private void print(Node x) {
+        if (x == null) return;
+        print(x.left);
+        System.out.println(x.key);
+        print(x.right);
+    }
+
+    private ArrayList<ArrayList<Node>> printGraph() {
+        Queue<Node> queue = new Queue<>();
+        ArrayList<ArrayList<Node>> listAll = new ArrayList<>();
+
+        queue.enqueue(root);
+        while (!queue.isEmpty()) {
+            int i = 0;
+            int len=queue.size();
+            ArrayList<Node> list = new ArrayList<Node>();
+            while (i<len) {
+
+                Node node = queue.dequeue();
+                list.add(node);
+//                System.out.println(node.key);
+                if (node.left != null) {
+                    queue.enqueue(node.left);
+                }
+                if (node.right != null) {
+                    queue.enqueue(node.right);
+                }
+                i += 1;
+            }
+            listAll.add(list);
+
+        }
+        return listAll;
+
+    }
+    public  void  println(){
+        ArrayList<ArrayList<Node>> arrayList = printGraph();
+        for (ArrayList<Node> list:arrayList
+        ) {
+            for (Node ele:list
+            ) {
+                System.out.print(ele.key+" ");
+            }
+            System.out.println("\n");
+        }
+    }
+
 
     public static void main(String[] args) {
         BST<String, Integer> bst = new BST<>();
@@ -160,14 +202,18 @@ public class BST<Key extends Comparable<Key>, Value> {
         bst.put("G", 3);
         bst.put("D", 1);
         bst.put("A", 1);
+        bst.println();
+//        bst.print();
 //        bst.deleteMin();
 //        System.out.println(bst.size());
 //        bst.deleteMax();
 //        System.out.println(bst.size());
 //        System.out.println(bst.min(bst.root).key);;
-        bst.delete("K");
-        System.out.println(bst.get("K")
-        );    }
+//        bst.delete("K");
+//        System.out.println(bst.get("K")
+//        );
+
+    }
 
 
 }
