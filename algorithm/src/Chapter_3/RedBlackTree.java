a04bb9e06016f49c43cefa77b1a3edc432efc4a1
dd <986494553@qq.com>
2018-09-10 21:31:15 +0800

    add bit black-red tree

--- algorithm/src/Chapter_3/RedBlackTree.java
+++ algorithm/src/Chapter_3/RedBlackTree.java
@@ -1,12 +1,14 @@
 package Chapter_3;
 
+import java.util.concurrent.BlockingDeque;
+
 public class RedBlackTree<Key extends Comparable<Key>,Value> {
     private  static final boolean RED=true;
     private  static final boolean BLACK=false;
-
+    private  Node root;
 
     private  class Node{
-        Key key;
+         Key key;
         Value value;
         Node left,right;
         int N;
@@ -18,7 +20,15 @@ public class RedBlackTree<Key extends Comparable<Key>,Value> {
             this.color = color;
         }
     }
+    private  int size(){
+        return size(root);
+    }
+    private int size(Node x){
+        if (x == null)return 0;
+        else  return x.N;
 
+
+    }
     private boolean isRed(Node x){
         if (x == null){
             return false;
@@ -26,7 +36,25 @@ public class RedBlackTree<Key extends Comparable<Key>,Value> {
         return x.color == RED;
     }
 
+    public void  put(Key key,Value val){
+        if (root ==null){
+            root = new Node(key,val,0,BLACK);
+        }
+        else root = put(root,key,val);
+    }
 
+    public Node put(Node x , Key key, Value val){
+        if (x== null){
+            return new Node(key,val,0,BLACK);
+        }
+        int cmp = key.compareTo(x.key);
+        if (cmp>0) x.right = put(x.right,key,val);
+        else if (cmp<0) x.left = put(x.left,key,val);
+        else x.value = val;
+        if(isRed(x))
+
+        x.N = size(x.left)+size(x.right)+1;
+    }
 
 
 }
