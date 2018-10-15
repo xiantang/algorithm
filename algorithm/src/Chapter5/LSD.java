372283f26dd22f71b0965b3fe4b3cc4a7b1782d2
dd <986494553@qq.com>
2018-10-15 11:04:13 +0800

    LSD.java

---
+++ algorithm/src/Chapter5/LSD.java
@@ -0,0 +1,42 @@
+package Chapter5;
+
+public class LSD {
+    public static void sort(String a[],int W){
+        int N = a.length;
+        int R = 256;
+
+        String[] aux = new String[N];
+        for (int d = W-1;d>=0;d--) {
+            int[] count = new int[R + 1]; //count是一个长度256的数组
+            for (int i = 0; i < N; i++) {
+                count[a[i].charAt(d) + 1]++;
+                //+1是为了转换为ASCII
+            }
+            for (int r = 0; r <R ; r++) {
+                count[r+1]+=count[r];
+            }
+
+            for (int i = 0; i <N ; i++) {
+                aux[count[a[i].charAt(d)]++] = a[i];
+            }
+            for (int i = 0; i <N ; i++) {
+                a[i] = aux[i];
+            }
+
+        }
+
+
+    }
+
+    public static void main(String[] args) {
+        String string[] = {"QASD","QWER","CDSA","EDFS","CFAA"};
+        int W = 4;
+        sort(string,W);
+        for (String s
+                :string
+             ) {
+            System.out.println(s);
+        }
+    }
+
+}
