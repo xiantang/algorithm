059b5bd34de3e802954e65b2d08fe1a21fb36f1a
dd <986494553@qq.com>
2018-10-18 20:00:43 +0800

    add put

--- algorithm/src/Chapter5/TrieST.java
+++ algorithm/src/Chapter5/TrieST.java
@@ -19,7 +19,18 @@ public class TrieST<Value> {
         char c = key.charAt(d);
         return get(x.next[c],key,d+1);
     }
-
+    private  void  put(String key,Value val){
+        root = put(root,key,val,0);
+    }
+    private Node put(Node x,String key,Value val,int d){
+        if (x == null) x = new Node();
+        if (d == key.length()){
+            x.val = val;
+            return x;
+        }
+        char c = key.charAt(d);
+        x.next[c] = put(x.next[c],key,val,d+1);
+    }
 
 
 }
