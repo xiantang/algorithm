6f89c246f24770fbe5a2eb4995da364214009945
dd <986494553@qq.com>
2018-10-30 17:29:51 +0800

    add readTrie

--- algorithm/src/Chapter5/Huffman.java
+++ algorithm/src/Chapter5/Huffman.java
@@ -1,5 +1,7 @@
 package Chapter5;
 
+import algs4.BinaryStdIn;
+
 public class Huffman {
     private static class Node implements Comparable<Node> {
         private char ch;
@@ -9,14 +11,13 @@ public class Huffman {
         public Node(char ch, int freq, Node left, Node right) {
             this.ch = ch;
             this.freq = freq;
-
-            //指向其他Node对象的引用
+            // 指向其他Node对象的引用
             this.left = left;
             this.right = right;
         }
 
         // 判断是否是叶子节点
-        public  boolean isLeaf(){
+        public boolean isLeaf() {
             return left == null && right == null;
 
         }
@@ -25,5 +26,17 @@ public class Huffman {
         public int compareTo(Node that) {
             return this.freq - that.freq;
         }
+    }
+    // 使用前缀码展开
+    public static void expand(){
+        Node root = readTrie();
+
+
+    }
+
+    private static Node readTrie() {
+        if (BinaryStdIn.readBoolean())
+            return new Node(BinaryStdIn.readChar(),0,null,null);
+        return new Node('\0',0,readTrie(),readTrie());
     }
 }
