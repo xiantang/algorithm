5e54665c4d70d3fc72ac5258abfa0bc81623ca7d
dd <986494553@qq.com>
2018-10-30 18:34:35 +0800

    expand

--- algorithm/src/Chapter5/Huffman.java
+++ algorithm/src/Chapter5/Huffman.java
@@ -1,6 +1,8 @@
 package Chapter5;
 
 import algs4.BinaryStdIn;
+import algs4.BinaryStdOut;
+import com.sun.tools.internal.ws.wsdl.document.BindingOutput;
 
 public class Huffman {
     private static class Node implements Comparable<Node> {
@@ -27,16 +29,28 @@ public class Huffman {
             return this.freq - that.freq;
         }
     }
-    // 使用前缀码展开
-    public static void expand(){
-        Node root = readTrie();
 
+    // 使用前缀码展开
+    public static void expand() {
+        // 读取单词查找树
+        Node root = readTrie();
+        int N = BinaryStdIn.readInt();
+        for (int i = 0; i <N ; i++) {
+            Node x = root;
+            // 迭代找到叶子节点的位置
+            while (x.isLeaf())
+                if (BinaryStdIn.readBoolean())
+                    x = x.right;
+                else x = x.left;
+            BinaryStdOut.write(x.ch);
+        }
+        BinaryStdOut.close();
 
     }
 
     private static Node readTrie() {
         if (BinaryStdIn.readBoolean())
-            return new Node(BinaryStdIn.readChar(),0,null,null);
-        return new Node('\0',0,readTrie(),readTrie());
+            return new Node(BinaryStdIn.readChar(), 0, null, null);
+        return new Node('\0', 0, readTrie(), readTrie());
     }
 }
