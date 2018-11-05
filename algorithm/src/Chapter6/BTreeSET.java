8424db6366560e819850ab6342a9a6e4c59113c0
dd <986494553@qq.com>
2018-11-05 14:30:57 +0800

    add BTree

--- algorithm/src/Chapter6/BTreeSET.java
+++ algorithm/src/Chapter6/BTreeSET.java
@@ -71,19 +71,66 @@ public class BTreeSET<Key extends Comparable<Key>, Value> {
     public Value search(Node x, Key key, int ht) {
         Entry[] children = x.children;
 
-        if (ht == 0){
-            for (int j = 0; j <x.m ; j++) {
-                if (eq(key,(Key)children[j].key))return (Value)children[j].val;
+        if (ht == 0) {
+            for (int j = 0; j < x.m; j++) {
+                if (eq(key, (Key) children[j].key)) return (Value) children[j].val;
             }
-        }
-        else {
-            for (int j = 0; j <x.m; j++) {
-                if (j+1 == x.m || less(key, (Key)children[j+1].key))
-                    return search(children[j].next, key, ht-1);
+        } else {
+            for (int j = 0; j < x.m; j++) {
+                if (j + 1 == x.m || less(key, (Key) children[j + 1].key))
+                    return search(children[j].next, key, ht - 1);
 
             }
         }
         return null;
+    }
+
+    private void put(Key key, Value val) {
+        if (key == null) throw new IllegalArgumentException("argument key to put() is null");
+        Node u = insert(root, key, val, height);
+
+
+    }
+
+    private Node insert(Node h, Key key, Value val, int ht) {
+        int j;
+        Entry t = new Entry(key, val, null);
+        // external node
+        if (ht == 0) {
+            for (j = 0; j < h.m; j++) {
+                if (less(key, (Key) h.children[j].key)) break;
+            }
+            
+        }
+        // internal node 
+        else {
+            for (j = 0; j < h.m; j++) {
+                
+                if ((j + 1 == h.m) || less(key, (Key) h.children[j + 1].key)) {
+                    Node u = insert(h.children[j++].next,key,val,ht-1);
+                    if (u == null) return null;
+                    t.key = u.children[0].key;
+                    t.next = u;
+                    break;
+                }
+            }
+        }
+        for (int i = h.m; i >j ; i--) 
+            h.children[i] = h.children[i-1];
+        h.children[j] = t;
+        h.m++;
+        if (h.m<M) return null;
+        else  return split(h);
+
+
+    }
+
+    private Node split(Node h) {
+        Node t = new Node(M/2);
+        h.m = M/2;
+        for (int j = 0; j <M/2 ; j++)
+            t.children[j] = h.children[M/2+j];
+        return t;
     }
 
     private boolean less(Key key, Key key1) {
